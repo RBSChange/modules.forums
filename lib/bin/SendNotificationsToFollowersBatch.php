04d051e6acad2c0ba7e9e80418a4828fd9889f0f
Gaël PORT <gael.port@rbs.fr>
2011-12-09 14:55:35 +0100

    [FIX #47866] Notification de suivi d'un sujet de forum et auteur d'un message (Évolution 44951)

--- lib/bin/SendNotificationsToFollowersBatch.php
+++ lib/bin/SendNotificationsToFollowersBatch.php
@@ -4,20 +4,24 @@ $rq = RequestContext::getInstance();
 $rq->setLang($rq->getDefaultLang());
 
 $ns = notification_NotificationService::getInstance();
-$thread = DocumentHelper::getDocumentInstance($threadId, 'modules_forums/thread');
+$thread = forums_persistentdocument_thread::getInstanceById($threadId);
 $ds = $thread->getDocumentService();
 $notif = $ns->getConfiguredByCodeName('modules_forums/follower', $ds->getWebsiteId($thread), $thread->getLang());
-$num = 1 + ($thread->getNbpost() - $thread->getTofollow()->getNumber());
 if ($notif instanceof notification_persistentdocument_notification)
 {
+	$num = 1 + ($thread->getNbpost() - $thread->getTofollow()->getNumber());
+	$lastAuthorId = $thread->getLastPost()->getAuthorid();
 	foreach ($thread->getFollowersArray() as $member)
 	{
 		$user = $member->getUser();
 		if ($user->isPublished())
 		{
-			$callback = array($ds, 'getNotificationParameters');
-			$params = array('thread' => $thread, 'member' => $member, 'specificParams' => array('NUM' => $num));
-			$user->getDocumentService()->sendNotificationToUserCallback($notif, $user, $callback, $params);
+			if ($lastAuthorId != $user->getId())
+			{
+				$callback = array($ds, 'getNotificationParameters');
+				$params = array('thread' => $thread, 'member' => $member, 'specificParams' => array('NUM' => $num));
+				$user->getDocumentService()->sendNotificationToUserCallback($notif, $user, $callback, $params);
+			}
 		}
 		else
 		{
